e2c4ae4a037c572cd3a58bd575764608052c65a3
sokny2023 <psnpsn667788@gmail.com>
2024-04-02 13:45:44 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="mt-6 text-center text-emerald-600 text-[50px] text-meduim">Hello Docker</h1>
+    </>
+  );
+}
